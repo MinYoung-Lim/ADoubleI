eb0edcb907a65a758a38b68b415d25c17647bdef
seunghyae <52480627+seunghyae@users.noreply.github.com>
2020-11-26 18:59:01 +0900

    //사진클릭해서 디테일화면 넘어가는 기능 추가  //디테일화면에 사진 띄우기 성공

--- app/src/main/java/com/example/adoublei/DetailActivity.java
+++ app/src/main/java/com/example/adoublei/DetailActivity.java
@@ -1,19 +1,104 @@
 package com.example.adoublei;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+
 public class DetailActivity extends AppCompatActivity {
+
+    private byte[] Byte_image;
+
+    private ImageView detailPhoto;
+    private TextView detailName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);  // layout xml 과 자바파일을 연결
 
-        Intent intent = new Intent(getBaseContext(),DetailActivity.class);
-    } // end onCreate()
+        detailPhoto = findViewById(R.id.detail_image);
+        detailName = findViewById(R.id.thename);
+        Intent intent = getIntent();
 
+        Uri photo = intent.getParcelableExtra("photo");
+        String title = intent.getStringExtra("title");
+
+        Bitmap ImgBitmap = null;
+
+        //ImgBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+        ImgBitmap = resize(getApplicationContext(), photo, 1000);
+
+        Byte_image = BitmapToByteArray(ImgBitmap);
+
+        detailPhoto.setImageBitmap(ImgBitmap);
+        detailName.setText(title);
+
+
+
+    } // end onCreate()
+    //Bitmap을 String형으로 변환
+    public static String BitmapToString(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 15, baos);
+        byte[] bytes = baos.toByteArray();
+        String temp = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return temp;
+    }
+
+
+    //Bitmap을 byte배열로 변환
+    public static byte[] BitmapToByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+        return baos.toByteArray();
+    }
+
+    //byte를 Bitmap으로 변환
+    public Bitmap byteArrayToBitmap( byte[] byteArray ) {
+        Bitmap bitmap = BitmapFactory.decodeByteArray( byteArray, 0, byteArray.length ) ;
+        return bitmap ;
+    }
+
+    private Bitmap resize(Context context, Uri uri, int resize) {
+        Bitmap resizeBitmap = null;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        try {
+            BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri), null, options); // 1번
+
+            int width = options.outWidth;
+            int height = options.outHeight;
+            int samplesize = 1;
+
+            while (true) {//2번
+                if (width / 2 < resize || height / 2 < resize)
+                    break;
+                width /= 2;
+                height /= 2;
+                samplesize *= 2;
+            }
+
+            options.inSampleSize = samplesize;
+            Bitmap bitmap = BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri), null, options); //3번
+            resizeBitmap = bitmap;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return resizeBitmap;
+
+    }
 
 
 }
